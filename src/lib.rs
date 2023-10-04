8f70daec3f051b583149202bfd155e9677ee44fc
Gaël <gael.batault@arche-mc2.fr>
2023-10-04 13:29:43 +0200

    states

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,33 @@
+#![allow(clippy::needless_pass_by_value)]
+
 extern crate wasm_bindgen;
 
-use wasm_bindgen::prelude::*;
 use bevy::prelude::*;
+use wasm_bindgen::prelude::*;
 
 use plugins::menu_plugin::MenuPlugin;
+use states::AppState;
 
 mod plugins;
+mod states;
 
 // ------ ------
 //     Start
 // ------ ------
 #[wasm_bindgen(start)]
 pub fn start() {
-	App::new()
-		.add_plugins((
-			DefaultPlugins.set(WindowPlugin {
-				primary_window: Some(Window {
-					resolution: Vec2::splat(512.).into(),
-					..default()
-				}),
-				..default()
-			}),
-			MenuPlugin
-		))
-		.insert_resource(ClearColor(Color::hex("aad9ff").unwrap()))
-		.run();
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    resolution: Vec2::splat(512.).into(),
+                    ..default()
+                }),
+                ..default()
+            }),
+            MenuPlugin,
+        ))
+        .add_state::<AppState>()
+        .insert_resource(ClearColor(Color::hex("aad9ff").unwrap_or_default()))
+        .run();
 }
--- src/plugins/menu_plugin.rs
+++ src/plugins/menu_plugin.rs
@@ -1,28 +1,31 @@
 use bevy::prelude::*;
 use seldom_pixel::prelude::*;
 
+use crate::states::AppState;
+
 pub struct MenuPlugin;
 
 impl Plugin for MenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(
-			PxPlugin::<Layer>::new(UVec2::splat(64), "/public/palette/palette.png".into()),
-		)
-		.add_systems(Startup, setup)
-		.add_systems(Update, interact_buttons);
+        app.add_plugins(PxPlugin::<Layer>::new(
+            UVec2::splat(64),
+            "/public/palette/palette.png".into(),
+        ))
+        .add_systems(OnEnter(AppState::MainMenu), setup)
+        .add_systems(OnExit(AppState::MainMenu), exit)
+        .add_systems(Update, interact_buttons);
     }
 }
-
 
 fn setup(
     mut commands: Commands,
     mut sprites: PxAssets<PxSprite>,
     mut filters: PxAssets<PxFilter>,
     mut cursor: ResMut<PxCursor>,
-	mut typefaces: PxAssets<PxTypeface>
+    mut typefaces: PxAssets<PxTypeface>,
 ) {
     commands.spawn(Camera2dBundle::default());
-    
+
     // Hide loader
     web_sys::window()
         .unwrap()
@@ -32,26 +35,25 @@ fn setup(
         .unwrap()
         .set_class_name("hide");
 
-	// Switch to an in-game cursor to show the player that they can click on things
-	let idle = filters.load("/public/filter/mouse.png");
-	*cursor = PxCursor::Filter {
+    // Switch to an in-game cursor to show the player that they can click on things
+    let idle = filters.load("/public/filter/mouse.png");
+    *cursor = PxCursor::Filter {
         idle: idle.clone(),
         left_click: filters.load("/public/filter/mouseclick.png"),
         right_click: idle,
     };
 
-	// Background
+    // Background
     let bkg = sprites.load("/public/sprite/bkg.png");
-    commands.spawn(
-        PxSpriteBundle ::<Layer> {
-            sprite: bkg,
-            position: IVec2::new(0, 0).into(),
-            ..Default::default()
-        });
-	
+    commands.spawn(PxSpriteBundle::<Layer> {
+        sprite: bkg,
+        position: IVec2::new(0, 0).into(),
+        ..Default::default()
+    });
+
     // Filter-based button
     let button_idle = sprites.load("/public/sprite/btn.png");
-	commands.spawn((
+    commands.spawn((
         PxSpriteBundle::<Layer> {
             sprite: button_idle,
             position: IVec2::new(32, 32).into(),
@@ -64,14 +66,15 @@ fn setup(
             click: filters.load("/public/filter/btnclick.png").into(),
         },
         Button,
+        Menu,
     ));
 
-	let typeface = typefaces.load_animated(
+    let typeface = typefaces.load_animated(
         "/public/typeface/anim_typeface.png",
         // See the function signature of `load_animated`
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().map(|character| {
-            (character, 2)
-        }),
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            .chars()
+            .map(|character| (character, 2)),
         // Equivalent to, for example, `vec![PxSeparatorConfig { character: ' ', width: 4 }]`
         [(' ', 4)],
     );
@@ -80,9 +83,9 @@ fn setup(
     commands.spawn((
         PxTextBundle::<Layer> {
             text: "ISL".into(),
-            typeface: typeface.clone(),
+            typeface,
             rect: IRect::new(IVec2::ZERO, IVec2::splat(64)).into(),
-            alignment: PxAnchor::Custom(Vec2::new(0.5,0.9)),
+            alignment: PxAnchor::Custom(Vec2::new(0.5, 0.9)),
             ..default()
         },
         PxAnimationBundle {
@@ -91,23 +94,28 @@ fn setup(
             on_finish: PxAnimationFinishBehavior::Loop,
             ..default()
         },
+        Menu,
     ));
-
 }
+
+#[derive(Component)]
+struct Menu;
 
 #[derive(Component)]
 struct Button;
 
 fn interact_buttons(
-    hovers: Query<(), (With<Button>, Added<PxHover>)>,
+    mut next_state: ResMut<NextState<AppState>>,
     clicks: Query<(), (With<Button>, Added<PxClick>)>,
 ) {
-    for _ in &hovers {
-        info!("Hover!");
-    }
-
     for _ in &clicks {
-        info!("Click!");
+        next_state.set(AppState::InGame);
+    }
+}
+
+fn exit(mut commands: Commands, query: Query<Entity, &Menu>) {
+    for e in query.iter() {
+        commands.entity(e).despawn();
     }
 }
 
