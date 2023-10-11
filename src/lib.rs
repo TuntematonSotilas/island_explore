c164628ed92e28b9ebe46762db0f6703c75cf9fd
Gaël <gael.batault@arche-mc2.fr>
2023-10-11 13:44:12 +0200

    resolution

--- src/lib.rs
+++ src/lib.rs
@@ -17,11 +17,18 @@ mod states;
 // ------ ------
 #[wasm_bindgen(start)]
 pub fn start() {
+    let (w, h) = get_window_size();
+    let size = if h > w {
+        w // Mobile
+    } else {
+        h * 0.8 // Desktop
+    };
+    
     App::new()
         .add_plugins((
             DefaultPlugins.set(WindowPlugin {
                 primary_window: Some(Window {
-                    resolution: Vec2::splat(512.).into(),
+                    resolution: Vec2::splat(size).into(),
                     ..default()
                 }),
                 ..default()
@@ -34,6 +41,24 @@ pub fn start() {
         .run();
 }
 
+#[allow(clippy::cast_possible_truncation)]
+fn get_window_size() -> (f32, f32) {
+    let w = web_sys::window()
+        .unwrap()
+        .inner_width()
+        .ok()
+        .unwrap()
+        .as_f64()
+        .unwrap() as f32;
+    let h = web_sys::window()
+        .unwrap()
+        .inner_height()
+        .ok()
+        .unwrap()
+        .as_f64()
+        .unwrap() as f32;
+    (w, h)
+}
 
 #[px_layer]
 struct Layer;
