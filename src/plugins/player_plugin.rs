1580967c9569f5f2dc55ddc6a08836c1c5767618
Gaël <gael.batault@arche-mc2.fr>
2024-02-05 17:02:53 +0100

    sprites

--- src/plugins/player_plugin.rs
+++ src/plugins/player_plugin.rs
@@ -20,7 +20,7 @@ impl Plugin for PlayerPlugin {
 }
 
 fn setup(mut commands: Commands, mut sprites: PxAssets<PxSprite>) {
-    let player = sprites.load("/public/sprite/player_b.png");
+    let player = sprites.load("/public/sprite/player.png");
     commands.spawn((
         PxSpriteBundle::<Layer> {
             sprite: player,
@@ -132,12 +132,12 @@ fn change_direction(
     if player.new_direct != player.prev_direct {
         
         info!("change direction");
-        let suffix = "t"; /*match player.new_direct {
+        let suffix = match player.new_direct {
             Direct::Right => "r",
             Direct::Left => "l",
             Direct::Top => "t",
             Direct::Bottom => "b",
-        };*/
+        };
         let path = format!("/public/sprite/player_{suffix}.png");
         let sprite = sprites.load_animated(path, 3);
         commands.entity(entity).despawn();
